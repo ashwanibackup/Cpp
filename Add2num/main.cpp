3d222adfccd3d856fc90203da88dd3592174420d
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-04-07 20:00:19 +0530

    1st commit

--- Add2num/main.cpp
+++ Add2num/main.cpp
@@ -8,5 +8,13 @@ using namespace std;
 
 int main()
 {
+  int num1{}, num2{}, sum{};
+
+  cout << "Enter the two number: "
+  cin >> num1 >> num2;
+
+  sum = num1 + num2;
+
+  cout << "Sum of " << num1 << " + " << num2 << " = " << sum <<endl;
   return 0;
 }
