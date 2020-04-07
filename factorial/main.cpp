a461a9d4c306b4e13f623848da03a1891a5100f0
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-04-07 19:45:00 +0530

    1st attempt code

--- factorial/main.cpp
+++ factorial/main.cpp
@@ -8,5 +8,27 @@ using namespace std;
 
 int main()
 {
+  unsigned int fact{1}, num{};
+
+  cout << "Enter the number: ";
+  cin >> num;
+
+  if(num < 0)
+  {
+    cout << "No factorial for negative number" << endl;
+  }
+  else if (num = 0)
+  {
+    cout << num << "! = 1" << endl;
+  }
+  else
+  {
+    while(num >= 1)
+    {
+      fact = fact * num--;
+    }
+    cout << num << "! = " << fact << endl;
+  }
+  
   return 0;
 }
